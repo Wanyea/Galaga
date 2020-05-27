25e8d034657e4282cdbdf3471adbc83cfaf20b49
BuildTools <unconfigured@null.spigotmc.org>
2020-05-27 14:01:00 -0400

    Initial Commit

---
+++ Unity/Assets/PlayerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//===============================================
+//Main Player Script - Connects all sub scripts
+//===============================================
+
+public class PlayerScript : MonoBehaviour {
+
+    //Reference all sub player scripts 
+    [SerializeField]
+    internal PlayerInputScript inputScript;
+
+    [SerializeField]
+    internal PlayerMovementScript movementScript;
+
+    [SerializeField]
+    internal PlayerCollisionScript collisonScript;
+
+  
+}
