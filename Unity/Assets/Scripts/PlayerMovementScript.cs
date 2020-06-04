b735e2040b849f44a481064beb302727b17e3a61
BuildTools <unconfigured@null.spigotmc.org>
2020-06-04 13:53:58 -0400

    Finished Player 2D Controller
    *Still some complications in Unity*

---
+++ Unity/Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//===============================================================
+//Player Movement Script - Deals with the movement of the player
+//===============================================================
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    //Reference to main player script
+    [SerializeField]
+    PlayerScript playerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerScript.inputScript.isLeftPressed) 
+        {
+            MovePlayerLeft();
+
+        } else if (playerScript.inputScript.isRightPressed) {
+            
+            MovePlayerRight();
+
+        } else {
+
+            StopMovement();
+        }
+        
+    }
+
+
+    private void MovePlayerLeft() {
+        //TODO: Create SerializedField where data can be entered via Unity 
+        playerScript.rb2d.velocity = new Vector2(-5, 0);
+    }
+
+    private void MovePlayerRight() {
+        //TODO: Create SerializedField where data can be entered via Unity 
+        playerScript.rb2d.velocity = new Vector2(5, 0);
+
+    }
+
+    private void StopMovement () {
+        playerScript.rb2d.velocity = new Vector2(0, 0);
+    }
+}
---
+++ Unity/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//===============================================
+//Main Player Script - Connects all sub scripts
+//===============================================
+
+public class PlayerScript : MonoBehaviour {
+
+    //Reference all sub player scripts 
+    [SerializeField]
+    internal PlayerInputScript inputScript;
+
+    [SerializeField]
+    internal PlayerMovementScript movementScript;
+
+    [SerializeField]
+    internal PlayerCollisionScript collisonScript;
+
+
+
+// Start is called before the first frame update
+void Start()
+{
+
+//anim = GetComponent<Animator>();
+rb2d = GetComponent<Rigidbody2D>();
+
+}
+
+// Update is called once per frame
+void Update() 
+{
+
+}
+
+    }   
+
