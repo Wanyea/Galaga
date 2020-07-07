d12189f3cfd220bb23166ddb5956cf97c7cee508
BuildTools <unconfigured@null.spigotmc.org>
2020-07-07 16:47:40 -0400

    EnemySpawnerScripy Added

--- Unity/Assets/Scripts/EnemyScript.cs
+++ Unity/Assets/Scripts/EnemyScript.cs
@@ -14,26 +14,11 @@ public class EnemyScript : MonoBehaviour
     public Collider2D collision;
     public Spline yellowSpline;
     public Spline redSpline;
-    public Transform yellowBlue; 
-    public Transform yellowBlue2;
-    public Transform yellowBlue3;
-    public Transform yellowBlue4;
-    public Transform whiteRed;
-    public Transform whiteRed2;
-    public Transform whiteRed3;
-    public Transform whiteRed4;
-
-    Object yellowBlueObject;
-    Object whiteRedObject;
-
     Object coord00;
 
     void Awake() 
     {
-        //Initiate first wave of enemies. 
-        spawnYellowEnemies();
-        spawnWhiteEnemies();
-            
+        
     }
 
     //Start is called before the first frame. 
@@ -41,15 +26,6 @@ public class EnemyScript : MonoBehaviour
     {
         animator = GetComponent<Animator>();
         rigidbody = GetComponent<Rigidbody2D>();
-        yellowBlueObject = Resources.Load("YellowWithBlueWings");
-        yellowBlueObject = Resources.Load("YellowWithBlueWings2");
-        yellowBlueObject = Resources.Load("YellowWithBlueWings3");
-        yellowBlueObject = Resources.Load("YellowWithBlueWings4");
-        whiteRedObject = Resources.Load("WhiteWithRedWings");
-        whiteRedObject = Resources.Load("WhiteWithRedWings2");
-        whiteRedObject = Resources.Load("WhiteWithRedWings3");
-        whiteRedObject = Resources.Load("WhiteWithRedWings4");
-        
 
     }
 
@@ -57,6 +33,7 @@ public class EnemyScript : MonoBehaviour
     void Update()
     {
         
+
     }
 
 //TODO: Destroy/ Move / change animation of enemies when hit with "Blaster" game object.
@@ -71,19 +48,19 @@ public class EnemyScript : MonoBehaviour
 
 //TODO: LERP to coordinate after enemies have finished their route on the spline. 
     public void spawnYellowEnemies() {
-            Tween.Spline(yellowSpline, yellowBlue, 0, 1, false, 2, 0, Tween.EaseInOut, Tween.LoopType.None);
-            Tween.Spline(yellowSpline, yellowBlue2, 0, 1, false, 2, 0.1f, Tween.EaseInOut, Tween.LoopType.None);
-            Tween.Spline(yellowSpline, yellowBlue3, 0, 1, false, 2, 0.2f, Tween.EaseInOut, Tween.LoopType.None);
-            Tween.Spline(yellowSpline, yellowBlue4, 0, 1, false, 2, 0.3f, Tween.EaseInOut, Tween.LoopType.None);
+            //Tween.Spline(yellowSpline, yellowBlue, 0, 1, false, 2, 0, Tween.EaseInOut, Tween.LoopType.None);
+            //Tween.Spline(yellowSpline, yellowBlue2, 0, 1, false, 2, 0.1f, Tween.EaseInOut, Tween.LoopType.None);
+            //Tween.Spline(yellowSpline, yellowBlue3, 0, 1, false, 2, 0.2f, Tween.EaseInOut, Tween.LoopType.None);
+            //Tween.Spline(yellowSpline, yellowBlue4, 0, 1, false, 2, 0.3f, Tween.EaseInOut, Tween.LoopType.None);
             //??: transform.position = Vector3.MoveTowards(coord00.x, coord00.y, 1.0f);
 
     }
 
     public void spawnWhiteEnemies(){
-            Tween.Spline(redSpline, whiteRed, 0, 1, false, 2, 0, Tween.EaseInOut, Tween.LoopType.None);
-            Tween.Spline(redSpline, whiteRed2, 0, 1, false, 2, 0.1f, Tween.EaseInOut, Tween.LoopType.None);
-            Tween.Spline(redSpline, whiteRed3, 0, 1, false, 2, 0.2f, Tween.EaseInOut, Tween.LoopType.None);
-            Tween.Spline(redSpline, whiteRed4, 0, 1, false, 2, 0.3f, Tween.EaseInOut, Tween.LoopType.None);
+            //Tween.Spline(redSpline, whiteRed, 0, 1, false, 2, 0, Tween.EaseInOut, Tween.LoopType.None);
+            //Tween.Spline(redSpline, whiteRed2, 0, 1, false, 2, 0.1f, Tween.EaseInOut, Tween.LoopType.None);
+            //Tween.Spline(redSpline, whiteRed3, 0, 1, false, 2, 0.2f, Tween.EaseInOut, Tween.LoopType.None);
+            //Tween.Spline(redSpline, whiteRed4, 0, 1, false, 2, 0.3f, Tween.EaseInOut, Tween.LoopType.None);
         
     }
 
